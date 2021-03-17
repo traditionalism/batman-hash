85be187c465f6ef3c3e9ca410d1b5f9305501b5c
Lucas <genevasterm@protonmail.ch>
2021-03-17 09:37:47 -0500

    refactor: re-wrote pretty much everything
    *this should be a good place to start for reading from a conf.toml file etc.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,40 @@
-use ggez::*;
-use ggez::{ContextBuilder, Context, GameResult, event};
-use ggez::event::{EventHandler};
+use ggez::conf::WindowMode;
+use ggez::{Context, GameResult};
+use ggez::{event, ContextBuilder};
+
+const WIDTH: f32 = 1920.0;
+const HEIGHT: f32 = 1080.0;
 
 struct Game {}
 
 impl Game {
-    pub fn new(_context: &mut Context) -> Game {
+    pub fn new(_ctx: &mut Context) -> Game {
         Game {}
     }
 }
 
 impl ggez::event::EventHandler for Game {
-    fn update(&mut self, _context: &mut Context) -> GameResult<()> {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
         Ok(())
     }
-    fn draw(&mut self, context: &mut Context) -> GameResult {
+    fn draw(&mut self, _ctx: &mut Context) -> GameResult {
         Ok(())
     }
 }
 
 
 pub fn main() {
-    let c = conf::Conf::new();
-    let (mut context, mut event_loop) = ContextBuilder::new("kiwigrape-matchmaking", "Lucas")
-        .conf(c)
+    let mut window_mode: WindowMode = Default::default();
+        window_mode.width = WIDTH;
+        window_mode.height = HEIGHT;
+    let (mut ctx, mut event_loop) = ContextBuilder::new("kiwigrape-matchmaking", "Lucas")
+        .window_mode(window_mode)
         .build()
         .unwrap();
-    
-    let mut game = Game::new(&mut context);    
-    
-    match event::run(&mut context, &mut event_loop, &mut game) {
+
+    let mut game = Game::new(&mut ctx);
+
+    match event::run(&mut ctx, &mut event_loop, &mut game) {
         Ok(_) => println!("Exited properly :)"),
         Err(e) => println!("The game had some problems: {}", e)
     }
